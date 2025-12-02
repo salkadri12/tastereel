0c1a45dfb7c172d66d24d33231f7d89e7bfbc59d
salkadri12 <salkadri@hotmail.com>
2025-12-02 12:19:05 +0200

    Add files via upload

---
+++ next.js.js
@@ -0,0 +1,21 @@
+{
+  "name": "tastereel",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "13.5.0",
+    "react": "18.2.0",
+    "react-dom": "18.2.0",
+    "@supabase/supabase-js": "^2.0.0",
+    "cloudinary": "^1.28.0",
+    "stripe": "^12.0.0",
+    "multer": "^1.4.5-lts.1",
+    "next-connect": "^0.12.2",
+    "streamifier": "^0.1.1"
+  }
+}
